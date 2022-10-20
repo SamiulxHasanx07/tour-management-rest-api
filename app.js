e4a1284c437c4f9fb3c4866bd3f1437acc126600
SamiulxHasanx07 <samiulxhasan650@gmail.com>
2022-10-20 18:58:46 +0600

    Deploy fix

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 
 // middlewares
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 //Routes
 const tourRoute = require("./routes/tour.route");
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const app = require("./app");
 
 // database connection
