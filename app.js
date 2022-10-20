fac4ed928209ef9801bae8c7d6c0e1d8234a0045
SamiulxHasanx07 <samiulxhasan650@gmail.com>
2022-10-20 19:03:21 +0600

    Deploy fix

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const cors = require("cors");
-require('dotenv').config();
 const app = express();
+const cors = require("cors");
 
 // middlewares
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 //Routes
 const tourRoute = require("./routes/tour.route");
